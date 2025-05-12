fb326948c8c95f8b995bf39ad184e8ea03c069da
AnshikaDasgutpata <ddasgupta06@gmail.com>
2025-05-12 21:55:02 +0600

    Add files via upload

---
+++ uuuu.py
@@ -0,0 +1,23 @@
+#loops . while loop
+print("helo")
+
+for i in range(0,100):
+ print(str(i)+"-helo")   
+
+ for i in range(10):
+  print("i")
+
+
+
+
+
+
+#list
+
+fruits = ["apple","banna","cherry","type shi"]
+
+print(fruits)
+print(fruits[0])
+
+for item in fruits:
+    print(item)
