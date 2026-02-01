b01eab72f7fb731d30a4a03668b2d8d57065e153
AnshikaDasgutpata <ddasgupta06@gmail.com>
2026-02-01 22:35:46 +0600

    Add files via upload

---
+++ gamewithpytho.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((400,500))
+pygame.display.set_caption("My first Pygame")
+
+done = False
+
+while not done :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            done = True
+    pygame.display.flip()
