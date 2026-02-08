ba3522aa35686aa0b33db061eb86986b3a05360d
AnshikaDasgutpata <ddasgupta06@gmail.com>
2026-02-08 22:32:32 +0600

    Add files via upload

---
+++ bluedvd.py
@@ -0,0 +1,16 @@
+import pygame
+
+screen = pygame.display.set_mode((500,500))
+pygame.display.set_caption("BLue..")
+
+done = False
+
+while not done :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.draw.rect(screen,(0,125,255), pygame.Rect(30,30,60,60))
+
+    pygame.display.flip()
+
